774c33d813f093c2945c66b8da1690d75804d704
abolfazltalebi <abolfazltalebii18@gmail.com>
2025-09-01 11:20:57 +0330

    Refactor imports in router and header components by removing unused React imports for cleaner code; streamline HeaderMobile and Naaavigation components.

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 import Layout from './components/Layout';
 import LoadingSpinner from './components/LoadingSpinner';
