22c4356957192008e1b8d7800b7f94215e94df0d
abolfazltalebi <abolfazltalebii18@gmail.com>
2025-09-24 22:32:45 +0330

    fixed and some changes

--- src/components/DentistCard.tsx
+++ src/components/DentistCard.tsx
@@ -3,25 +3,26 @@ import Instagram from "./icons/Instagram";
 import Linkdin from "./icons/Linkdin";
 import Twiter from "./icons/Twiter";
 type DentistCardProps = {
-    img: string;
-    name: string;
-    desc: string;
-  };
-export default function DentistCard({ img, name, desc }:DentistCardProps) {
+  img: string;
+  name: string;
+  desc: string;
+};
+export default function DentistCard({ img, name, desc }: DentistCardProps) {
   return (
     <div className="relative group max-w-[250px] mx-auto">
       <img src={img} alt={name} className="w-full rounded-lg" />
       <div
-        className="relative bg-white bottom-[50px] z-10 h-[86px] group-hover:h-[140px] 
-                 overflow-hidden p-4 rounded-2xl text-center 
-                 transition-all duration-700 shadow-md"
+        className="relative liquid-glass bottom-[50px] group-hover:bottom-[100px] z-10 
+             h-[86px] group-hover:h-[190px] overflow-hidden 
+             p-4 rounded-2xl border-2 border-white/50 text-center 
+             transition-all duration-700 shadow-md"
       >
-        <h2 className="font-bold uppercase">{name}</h2>
-        <p className="text-zinc-800">{desc}</p>
+        <h2 className="font-bold uppercase text-[#3366FF] bg-white rounded-2xl py-2 my-2">{name}</h2>
+        <p className="text-[#3366FF] bg-white rounded-2xl text-sm py-2">{desc}</p>
         <div
           className="flex items-center justify-between mt-4 
-                   opacity-0 scale-90 group-hover:opacity-100 group-hover:scale-100 
-                   transition-all duration-700"
+               opacity-0 scale-90 group-hover:opacity-100 group-hover:scale-100 
+               transition-all duration-700"
         >
           <Linkdin />
           <Instagram />
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,12 @@
-
+import Logo from "../icons/Logo"
 export default function Footer() {
   return (
-    <footer className="bg-white border-t border-gray-200 mt-auto">
+    <footer className="bg-[#C7F0FD] py-14">
     <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-      <div className="text-center text-gray-500 text-sm">
-        © 2024 Confident.co. All rights reserved.
+      <div>
+        <div>
+          <Logo/>
+        </div>
       </div>
     </div>
   </footer>
