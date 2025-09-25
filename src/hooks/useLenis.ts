66724c005a4e88f35443311420656dcc3c0b6b4b
abolfazltalebi <abolfazltalebii18@gmail.com>
2025-09-25 11:35:58 +0330

    some changes

--- src/hooks/useLenis.ts
+++ src/hooks/useLenis.ts
@@ -4,9 +4,9 @@ import Lenis from "@studio-freight/lenis";
 export function useLenis() {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2, // مدت زمان انیمیشن
-      lerp: 0.1,     // ضریب هموارسازی (0.0 تا 1.0)
-      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // تابع easing
+      duration: 1.2,
+      lerp: 0.1,  
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), 
     });
 
     let rafId: number;
